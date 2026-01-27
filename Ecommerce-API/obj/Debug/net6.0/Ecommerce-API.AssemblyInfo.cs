311ecaaac8591583294b05400b5d0aef17606e59
Marcos Vogado <marcoshcvogado@gmail.com>
2026-01-27 17:11:56 -0300

    Setup chunks

--- Ecommerce-API/obj/Debug/net6.0/Ecommerce-API.AssemblyInfo.cs
+++ Ecommerce-API/obj/Debug/net6.0/Ecommerce-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7be0d021d9fca2ceb59764cc48f20e63cbff976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b50598265321953dcbf5c612efbcfebe6c3521d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

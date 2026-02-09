e7644101b1d07d8c2321865ecdd1e5bf92c97340
Marcos Vogado <marcoshcvogado@gmail.com>
2026-02-09 14:12:38 -0300

    add

--- Ecommerce-API/Menus/LinqMenu.cs
+++ Ecommerce-API/Menus/LinqMenu.cs
@@ -18,10 +18,19 @@ namespace Ecommerce_API.Menus
             Console.WriteLine("> Digite -1 para sair");
             Console.WriteLine("\n=============================================");
             string userInput = (Console.ReadLine() ?? "").Trim().ToLower();
+            int option = int.Parse(userInput);
 
             switch (option)
             {
-                case
+                case 1: 
+                    Console.Write("Digite a categoria para filtrar: ");
+                    string category = Console.ReadLine() ?? "";
+                    // Aqui você pode implementar a lógica para filtrar os produtos por categoria usando LINQ
+                    Console.WriteLine($"Filtrando produtos pela categoria: {category}");
+                    break;
+                case 2:
+
+                    break;
             }
         }
     }
--- Ecommerce-API/obj/Debug/net6.0/Ecommerce-API.AssemblyInfo.cs
+++ Ecommerce-API/obj/Debug/net6.0/Ecommerce-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c4bb0345397079d1e95be660d8b7e53e2bb3b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f96216122246f2673d37ea64b835270cc4ce4f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
